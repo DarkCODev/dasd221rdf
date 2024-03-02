33995dc56d113149a2c199c8b23d3c6ccc97802c
M4xH3adR00m <owlt3am@proton.me>
2024-03-02 15:34:52 +0100

    Ajustada Splash Screen a AOPS 12

--- app/src/main/java/com/loseweight/SplashScreenActivity.kt
+++ app/src/main/java/com/loseweight/SplashScreenActivity.kt
@@ -1,91 +1,80 @@
 package com.loseweight
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
+import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
-import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.ads.AdSettings
 import com.loseweight.facebookad.AudienceNetworkInitializeHelper
 import com.loseweight.utils.Constant
 import com.loseweight.utils.Utils
 import com.utillity.db.DataHelper
 
+class SplashScreenActivity : AppCompatActivity() {
 
-class SplashScreenActivity : BaseActivity() {
+    private val handler = Handler()
 
-    internal var handler = Handler()
-    @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Inicializa la nueva API SplashScreen antes de llamar a super.onCreate y setContentView
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
+        // Inicialización de Audience Network y otras inicializaciones
         AudienceNetworkInitializeHelper.initialize(this)
-        setContentView(R.layout.activity_splash_screen)
+
+        // Comprobación de la base de datos y otras operaciones de inicio
         DataHelper(this).checkDBExist()
 
+        // Log para imprimir el hash key, útil para configuraciones de Facebook u otros SDKs
         Utils.printHashKey(this)
 
+        // Programar la decisión de qué actividad iniciar después de la SplashScreen
+        handler.postDelayed({
+            decideNextActivity()
+        }, 1000) // Suponiendo un retardo de 2 segundos para demostración
     }
 
+    private fun decideNextActivity() {
+        // Elimina cualquier callback pendiente para evitar lanzar la actividad más de una vez
+        handler.removeCallbacksAndMessages(null)
 
-    fun showAlertDialog() {
-        val builder = AlertDialog.Builder(getActivity(), R.style.MyAlertDialogStyle)
-        builder.setTitle("Important")
-            .setMessage(getActivity().resources.getString(R.string.permission_msg))
-            .setPositiveButton(android.R.string.ok) { dialog, which -> dialog.dismiss() }
-            .setNegativeButton(getActivity().resources.getString(R.string.permission_setting)) { dialog, which ->
-                dialog.dismiss()
-                finish()
-                val intent = Intent()
-                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }.show()
+        // Aquí tu lógica existente para decidir qué actividad iniciar, simplificada para el ejemplo
+        val isFirstTime = Utils.getPref(this, Constant.PREF_IS_FIRST_TIME, true)
+        val goalIsEmpty = Utils.getPref(this, Constant.PREF_GOAL, "").isNullOrEmpty()
 
-    }
-
-    fun startapp(sleepTime: Long) {
-        handler.postDelayed(startApp, sleepTime)
-    }
-
-    internal var startApp: Runnable = object : Runnable {
-        override fun run() {
-            handler.removeCallbacks(this)
-
-            if (Utils.getPref(
-                    this@SplashScreenActivity,
-                    Constant.PREF_IS_FIRST_TIME,
-                    true
-                ) && Utils.getPref(this@SplashScreenActivity, Constant.PREF_GOAL, "")
-                    .isNullOrEmpty()
-            ) {
-                val i = Intent(getActivity(), ChooseYourPlanActivity::class.java)
-                i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                startActivity(i)
-                finish()
-            } else {
-                val i = Intent(getActivity(), HomeActivity::class.java)
-
-                if(intent.extras != null){
-                    if(intent.extras!!.containsKey("isFrom") && intent.extras!!.get("isFrom") == Constant.FROM_DRINK_NOTIFICATION)
-                    {
-                       i.putExtra("isFrom",Constant.FROM_DRINK_NOTIFICATION)
-                    }
+        val intent = if (isFirstTime && goalIsEmpty) {
+            Intent(this, ChooseYourPlanActivity::class.java)
+        } else {
+            Intent(this, HomeActivity::class.java).apply {
+                if (intent.extras?.containsKey("isFrom") == true && intent.extras!!.get("isFrom") == Constant.FROM_DRINK_NOTIFICATION) {
+                    putExtra("isFrom", Constant.FROM_DRINK_NOTIFICATION)
                 }
-                i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                startActivity(i)
-                finish()
             }
-
-            /*val i = Intent(getActivity(), ChooseYourPlanActivity::class.java)
-            i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(i)
-            finish()*/
         }
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
     }
 
+
+
+    fun startapp(userStatus: Int) {
+        val intent = when (userStatus) {
+            0 -> Intent(this, ChooseYourPlanActivity::class.java)
+            1 -> Intent(this, HomeActivity::class.java)
+            else -> return // Si el estado no es reconocido, no hace nada (o maneja según sea necesario)
+        }
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+    }
 
 }
+
+
